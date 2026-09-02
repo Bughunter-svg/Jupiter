cba4daedcb53ccfcd27343c2a9410e8f6ad3e4fd
harshu-wtf <bugghntr@gmail.com>
2026-09-02 18:56:35 +0530

    Refactor ISR timer and keyboard handlers
    Refactor ISR handlers to remove unnecessary comments and streamline code.

--- myos/kernel/isr.asm
+++ myos/kernel/isr.asm
@@ -1,32 +1,21 @@
-; kernel/isr.asm
 global isr_timer
+global isr_keyboard
+
+extern timer_handler
+extern keyboard_handler
 
 isr_timer:
-    ; ACKNOWLEDGE FIRST!
+    pusha
+    call timer_handler
     mov al, 0x20
     out 0x20, al
-    
-    ; DEBUG: Print single character
-    pusha
-    mov edi, 0xB8000 + 160  ; Second line
-    mov byte [edi], 'T'
-    mov byte [edi+1], 0x2A
     popa
-    
     iret
-; Add this after isr_timer
-global isr_keyboard
 
 isr_keyboard:
     pusha
-    ; Acknowledge interrupt FIRST
+    call keyboard_handler
     mov al, 0x20
     out 0x20, al
-    out 0xA0, al
-    
-    ; Call C keyboard handler
-    extern keyboard_handler
-    call keyboard_handler
-    
     popa
     iret
