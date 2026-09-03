d8434ed5cc95bad622e07deadd98560f86b546e7
harshu-wtf <bugghntr@gmail.com>
2026-09-03 11:34:39 +0530

    Update keyboard.h

--- myos/kernel/keyboard.h
+++ myos/kernel/keyboard.h
@@ -6,16 +6,18 @@
 #define KEY_LEFT 0x82
 #define KEY_RIGHT 0x83
 
-// Add these extern declarations
+#define KEY_CTRL_S 0x13
+#define KEY_CTRL_Q 0x11
+
 extern char keyboard_buffer[256];
 extern int keyboard_buffer_size;
 
 int get_key();
 void get_line(char* buffer, int size);
 void get_init();
-int key_available();          // Check if key is available
-char keyboard_pop();          // Get key from buffer
-void init_keyboard();         // Initialize keyboard
-void keyboard_handler(); 
+int key_available();
+char keyboard_pop();
+void init_keyboard();
+void keyboard_handler();
 
 #endif
