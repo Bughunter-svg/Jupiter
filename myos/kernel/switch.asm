20990ba47f10eb41708aec9c9dee9f177e2876c5
harshu-wtf <bugghntr@gmail.com>
2026-09-03 14:04:29 +0530

    Enhance switch_task_asm for better context handling
    Refactor context saving and restoring in switch_task_asm for improved register management.

--- myos/kernel/switch.asm
+++ myos/kernel/switch.asm
@@ -2,24 +2,41 @@ section .text
 global switch_task_asm
 
 switch_task_asm:
-    mov eax, [esp + 4]
-    mov edx, [esp + 8]
+    pushad
 
+    mov eax, [esp + 36]
+    mov edx, [esp + 40]
+
+    mov [eax + 4], ebx
+    mov [eax + 8], ecx
+    mov [eax + 12], edx
+    mov [eax + 16], esi
+    mov [eax + 20], edi
     mov [eax + 24], esp
     mov [eax + 28], ebp
 
-    mov ecx, [esp]
+    mov ecx, [esp + 32]
     mov [eax + 32], ecx
 
     pushf
     pop ecx
     mov [eax + 36], ecx
 
-    mov esp, [edx + 24]
-    mov ebp, [edx + 28]
+    mov eax, [esp + 40]
 
-    push dword [edx + 36]
+    mov ebx, [eax + 4]
+    mov ecx, [eax + 8]
+    mov edx, [eax + 12]
+    mov esi, [eax + 16]
+    mov edi, [eax + 20]
+    mov esp, [eax + 24]
+    mov ebp, [eax + 28]
+
+    push dword [eax + 36]
     popf
 
-    push dword [edx + 32]
+    mov ecx, [eax + 32]
+    push ecx
+
+    popad
     ret
