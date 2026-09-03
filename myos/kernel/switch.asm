29280c8255be3c42261361800f19ac8bc2c7b49d
harshu-wtf <bugghntr@gmail.com>
2026-09-03 13:27:05 +0530

    Enhance task switching in switch.asm
    Refactor context switching to save and restore additional registers and flags.

--- myos/kernel/switch.asm
+++ myos/kernel/switch.asm
@@ -1,37 +1,37 @@
-; kernel/switch.asm
 section .text
 global switch_task_asm
 
 switch_task_asm:
-    ; Save current process state
-    mov eax, [esp + 4]  ; current PCB pointer
-    mov [eax + 0], ebx
-    mov [eax + 4], ecx
-    mov [eax + 8], edx
-    mov [eax + 12], esi
-    mov [eax + 16], edi
-    mov [eax + 20], esp
-    mov [eax + 24], ebp
-    
-    ; Save instruction pointer and flags
+    mov eax, [esp + 4]
+
+    mov [eax + 0], eax
+    mov [eax + 4], ebx
+    mov [eax + 8], ecx
+    mov [eax + 12], edx
+    mov [eax + 16], esi
+    mov [eax + 20], edi
+    mov [eax + 24], esp
+    mov [eax + 28], ebp
+
+    mov ebx, [esp]
+    mov [eax + 32], ebx
+
     pushf
-    pop dword [eax + 32]  ; eflags
-    mov ebx, [esp]        ; return address (eip)
-    mov [eax + 28], ebx   ; eip
-    
-    ; Load next process state
-    mov eax, [esp + 8]    ; next PCB pointer
-    mov ebx, [eax + 0]
-    mov ecx, [eax + 4]
-    mov edx, [eax + 8]
-    mov esi, [eax + 12]
-    mov edi, [eax + 16]
-    mov esp, [eax + 20]
-    mov ebp, [eax + 24]
-    
-    ; Load instruction pointer and flags
-    push dword [eax + 32] ; eflags
+    pop ebx
+    mov [eax + 36], ebx
+
+    mov eax, [esp + 8]
+
+    mov ebx, [eax + 4]
+    mov ecx, [eax + 8]
+    mov edx, [eax + 12]
+    mov esi, [eax + 16]
+    mov edi, [eax + 20]
+    mov esp, [eax + 24]
+    mov ebp, [eax + 28]
+
+    push dword [eax + 36]
     popf
-    push dword [eax + 28] ; eip
-    
+
+    push dword [eax + 32]
     ret
