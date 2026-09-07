a509154adda14b95c3f983e8200893217ef0a8bc
harshu-wtf <bugghntr@gmail.com>
2026-09-07 19:23:15 +0530

    Add MAX_MEMORY_REGIONS and MemoryRegion struct

--- myos/kernel/memory.h
+++ myos/kernel/memory.h
@@ -7,11 +7,16 @@
 #define HEAP_START 0x100000U
 #define MIN_HEAP_SIZE 0x400000U
 
+#define MAX_MEMORY_REGIONS 32
 #define PAGE_SIZE 4096U
-#define MAX_PHYSICAL_MEMORY 0x100000000ULL
 #define MAX_PHYSICAL_PAGES 1048576U
 #define BITMAP_SIZE (MAX_PHYSICAL_PAGES / 8)
 
+typedef struct {
+    uint64_t base;
+    uint64_t length;
+    uint32_t type;
+} MemoryRegion;
 void mem_init(void);
 void mem_set_total(size_t total);
 void mem_detect_multiboot(unsigned int *mb_info);
